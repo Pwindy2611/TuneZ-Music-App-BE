e70f2582640faa67af7afe405b5b3eb4af63a5b2
rena2302 <khoivu2302@gmail.com>
2025-01-07 22:10:35 +0700

    update#1

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@ import bodyParser from 'body-parser';
 import compression from 'compression';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import * as dotenv from 'dotenv';
 import express from 'express';
+import admin from 'firebase-admin';
 import http from 'http';
+
 
 const app = express();
 
@@ -19,4 +22,22 @@ const server = http.createServer(app);
 
 server.listen(8080, () => {
     console.log("Server Running on http://localhost:8080/")
+});
+
+dotenv.config();
+
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
+
+admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL: firebaseConfig.databaseURL,
 });
