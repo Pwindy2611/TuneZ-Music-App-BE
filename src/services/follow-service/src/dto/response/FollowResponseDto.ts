a6ca09f86e9d3c6f604187de560c80da04d887e3
Vũ Lê Anh Khôi <khoivu2302@gmail.com>
2025-03-17 22:54:00 +0700

    follow-service: add id in response

--- src/services/follow-service/src/dto/response/FollowResponseDto.ts
+++ src/services/follow-service/src/dto/response/FollowResponseDto.ts
@@ -1,9 +1,11 @@
 export class FollowResponseDto {
+    id: string;
     name: string;
     profileImage: string;
     followerCount: number;
 
-    constructor(name: string, followerCount: number, profileImage: string){
+    constructor(id: string, name: string, followerCount: number, profileImage: string){
+        this.id = id;
         this.name = name;
         this.followerCount = followerCount;
         this.profileImage = profileImage;
