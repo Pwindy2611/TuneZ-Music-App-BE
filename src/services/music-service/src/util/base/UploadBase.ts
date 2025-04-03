a48baec51b8377b257ef1927e8a5002274b81d6f
Vũ Lê Anh Khôi <khoivu2302@gmail.com>
2025-04-03 17:04:32 +0700

    music-service: fix delete music

--- src/services/music-service/src/util/base/UploadBase.ts
+++ src/services/music-service/src/util/base/UploadBase.ts
@@ -19,10 +19,16 @@ class UploadBase {
 
     uploadToCloudinary = async (file: { originalName: string; buffer: Buffer }, musicId: string): Promise<string> => {
         return new Promise((resolve, reject) => {
+
+            const sanitizedFileName = file.originalName
+                .replace(/\.[^/.]+$/, "")
+                .replace(/[^a-zA-Z0-9_-]/g, "")
+                .toLowerCase();
+
             const uploadStream = cloudinaryInstance.uploader.upload_stream(
                 {
                     folder: `music-storage/files/${musicId}`,
-                    public_id: file.originalName.replace(/\.[^/.]+$/, ""),
+                    public_id: sanitizedFileName,
                     resource_type: "auto"
                 },
                 (error, result) => {
@@ -51,7 +57,31 @@ class UploadBase {
         const uploadData = await this.uploadFile(file, id);
         return await this.getSignedFileUrl(uploadData);
     }
+    async  deleteFolder(musicId: string) {
+        const folderPath = `music-storage/files/${musicId}`;
 
+        const fileTypes = ["image", "video", "raw"];
+        let resourcesByType: { [key: string]: string[] } = { image: [], video: [], raw: [] };
+
+        for (const type of fileTypes) {
+            const resources = await cloudinaryInstance.api.resources({
+                type: "upload",
+                prefix: folderPath,
+                resource_type: type,
+                max_results: 500,
+            });
+
+            resourcesByType[type] = resources.resources.map((file: any) => file.public_id);
+        }
+
+        for (const type of fileTypes) {
+            if (resourcesByType[type].length > 0) {
+                await cloudinaryInstance.api.delete_resources(resourcesByType[type], { resource_type: type });
+            }
+        }
+
+        await cloudinaryInstance.api.delete_folder(folderPath);
+    }
 }
 
 export default new UploadBase();
